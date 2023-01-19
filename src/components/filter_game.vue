db2c7962a2f7e2d5bbfce083ae3e982c3c79b22c
Ademaru <58119776+denis-muhortov@users.noreply.github.com>
2023-01-19 21:42:13 +0300

    fix

--- src/components/filter_game.vue
+++ src/components/filter_game.vue
@@ -109,15 +109,16 @@ export default {
 
 <style scoped>
 .slider{
+    margin: 20px 0px 0px 0px;
     width: 280px;
 }
 .noUi-target{
     border: 1px solid #F5A516;
     background: #F5A51660;
     box-shadow: none;
-}
-.noUi-state-tap{
-    background: #F5A51680;
+    font-size: 18px;
+    color: var(--vt-c-white);
+    font-family: 'TheAncient', 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 }
 .container_filter{
     position: fixed;
