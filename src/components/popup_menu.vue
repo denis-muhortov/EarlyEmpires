266fff1b0f2a057fe86edfed2a225a1dc914dd5e
SenSkysh <kukukumaks@gmail.com>
2023-01-20 19:11:49 +0300

    sentry

--- src/components/popup_menu.vue
+++ src/components/popup_menu.vue
@@ -26,7 +26,8 @@ export default {
         changepage(page) {
             this.togglepopup();
             this.$emit('selectTab', page);
-        }
+        },
+        
     },
     computed: {
         username() {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import * as Sentry from "@sentry/vue";
 
 import toaster from "./toaster/index";
 
@@ -12,6 +13,35 @@ window.global = window;
 const pinia = createPinia();
 const app = createApp(App);
 
+
+
+
+
+Sentry.init({
+    app,
+    dsn: "https://4d6e447a637d45a2bcdae572aa374e6b@o4504537608290304.ingest.sentry.io/4504537611567104",
+    debug: true,
+    integrations: [],
+    //allowUrls:['*'],
+    tracingOrigins :['*'],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+    logErrors: true,
+    release: '0.0.1'
+  });
+
+
+
+
+
+
 app.use(pinia);
 app.use(toaster);
+
+
+
+
+
 app.mount("#app");
