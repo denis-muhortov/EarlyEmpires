31f7139b98619feac7749639fc9247f37053a9d6
SenSkysh <kukukumaks@gmail.com>
2023-01-20 19:28:55 +0300

    unclaimed

--- src/main.js
+++ src/main.js
@@ -22,14 +22,14 @@ Sentry.init({
     dsn: "https://4d6e447a637d45a2bcdae572aa374e6b@o4504537608290304.ingest.sentry.io/4504537611567104",
     debug: true,
     integrations: [],
-    //allowUrls:['*'],
-    tracingOrigins :['*'],
+    allowUrls:['earlyempire-nft.com'],
+    //tracingOrigins :['*'],
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
     tracesSampleRate: 1.0,
     logErrors: true,
-    release: '0.0.1'
+    release: '0.0.2'
   });
 
 
