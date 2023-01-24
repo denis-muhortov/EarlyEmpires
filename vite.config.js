c6b910a423f3e69a9a1927b29f5ad7e0fb7b7edb
SenSkysh <kukukumaks@gmail.com>
2023-01-24 12:24:17 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 
-const RELEASE_VER = "0.9.5";
+const RELEASE_VER = "1.0.1";
 
 // https://vitejs.dev/config/
 export default defineConfig({
